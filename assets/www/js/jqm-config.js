c77425f0938c4fac7bc3c711cb703d3f1575c1aa
vincent <vincent@zescore.com>
2013-02-16 19:54:39 +0100

    new cordova

--- assets/www/js/jqm-config.js
+++ assets/www/js/jqm-config.js
@@ -1,31 +1,31 @@
-$(document).bind("mobileinit", function () {
-    $.mobile.ajaxEnabled = false;
-    $.mobile.linkBindingEnabled = false;
-    $.mobile.hashListeningEnabled = false;
-    $.mobile.pushStateEnabled = false;
-    $.mobile.allowCrossDomainPages = true;
-    //DESACTIVATE INIT FOR DOM
-    //$.mobile.autoInitializePage = false;
-    
-    //$.mobile.defaultPageTransition = 'none';    
-    //$.mobile.autoInitializePage = false;
-    //$.mobile.touchOverflowEnabled = false;
-    //$.mobile.defaultDialogTransition = 'none';
-    //$.mobile.loadingMessage = 'Daten werden geladen...' ;
-    //$.mobile.slideText:"none";
-    //$.mobile.slideUpText:"none";
-    
-    $.extend($.mobile, {
-      //autoInitializePage : false,
-      touchOverflowEnabled : false,
-      slideText :  "none",
-      slideUpText :  "none",
-      defaultPageTransition:"none",
-      defaultDialogTransition:"none"
-    });
+$(document).bind("mobileinit", function() {
+  $.mobile.ajaxEnabled = false;
+  $.mobile.linkBindingEnabled = false;
+  $.mobile.hashListeningEnabled = false;
+  $.mobile.pushStateEnabled = false;
+  $.mobile.allowCrossDomainPages = true;
+  // DESACTIVATE INIT FOR DOM
+  // $.mobile.autoInitializePage = false;
 
-    // Remove page from DOM when it's being replaced
-    //$('div[data-role="page"]').live('pagehide', function (event, ui) {
-    //    $(event.currentTarget).remove();
-    //});
+  // $.mobile.defaultPageTransition = 'none';
+  // $.mobile.autoInitializePage = false;
+  // $.mobile.touchOverflowEnabled = false;
+  // $.mobile.defaultDialogTransition = 'none';
+  // $.mobile.loadingMessage = 'Daten werden geladen...' ;
+  // $.mobile.slideText:"none";
+  // $.mobile.slideUpText:"none";
+
+  $.extend($.mobile, {
+    // autoInitializePage : false,
+    touchOverflowEnabled : false,
+    slideText : "none",
+    slideUpText : "none",
+    defaultPageTransition : "none",
+    defaultDialogTransition : "none"
+  });
+
+  // Remove page from DOM when it's being replaced
+  // $('div[data-role="page"]').live('pagehide', function (event, ui) {
+  // $(event.currentTarget).remove();
+  // });
 });

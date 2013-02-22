9ed99e70abd01eb70f33513af574e8929b3bb3f9
Marc Dassonneville <marc.dassonneville@gmail.com>
2013-02-22 14:41:53 +0100

    empty startup func

--- assets/www/js/y/stats.js
+++ assets/www/js/y/stats.js
@@ -1,11 +1,14 @@
 (function (Y, undefined) {
-
   /*
    * Api:
    *  Y.Stats.click(ev, 'button:les_plus');
    *  Y.Stats.page(from, to);
    */
   var Stats = {
+    startup: function () {
+      
+    },
+
     clic: function (ev, data) {
       
     },

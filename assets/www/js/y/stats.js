3a7544019ff4a0a89e54c0e0583f1fbddadf4d5e
Marc Dassonneville <marc.dassonneville@gmail.com>
2013-02-22 13:59:06 +0100

    Y.Stats: nothing yet.

---
+++ assets/www/js/y/stats.js
@@ -0,0 +1,23 @@
+(function (Y, undefined) {
+
+  /*
+   * Api:
+   *  Y.Stats.click(ev, 'button:les_plus');
+   *  Y.Stats.page(from, to);
+   */
+  var Stats = {
+    clic: function (ev, data) {
+      
+    },
+
+    page: function (from, to) {
+      
+    },
+
+    send: function (line) {
+      
+    }
+  };
+
+  Y.Stats = Stats;
+})(Y);

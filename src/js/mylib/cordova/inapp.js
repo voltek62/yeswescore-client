f63542549884b7034371432df488cd09a887c3d2
Marc Dassonneville <marc.dassonneville@gmail.com>
2013-03-14 11:49:52 +0100

    cordova: inappbrowser: working example

--- src/js/mylib/cordova/inapp.js
+++ src/js/mylib/cordova/inapp.js
@@ -1,70 +1,51 @@
  (function (Cordova, undefined) {
-  "use strict";
+   "use strict";
 
    var iabRef = null;
 
    var InApp = {
-                                                          
-                        
-    launchUrl: function(url) {
-    
-        try {
-                // target : _blank, _self, _system
-            var target = "_system";
-            
-            iabRef = window.open(url, target, "location=no");
-            
-            console.log('INAPP addEventListener',onLoadStart);
-            
-            iabRef.addEventListener('loadstart', onLoadStart);
-            iabRef.addEventListener('loadstart', onLoadStop);
-            iabRef.addEventListener('exit', onExit);
-        
-        } catch (e) {
-            console.log('INAPP erreur '+e+' pour '+url);
-        }
-    
-    },
-    
-          
-    onLoadStart: function (event) {
-        console.log('INAPP ' + event.type + ' - ' + event.url);
-        
-        //si url #success ou # error on kill
-        if (event.url.indexOf("success")!=-1 || event.url.indexOf("error")!=-1) {       
-                console.log("INAPP SPECIAL DETECTE");  
-                
-                iabRef.close();         
-        }
-        
-        
-    },
+     launchUrl: function (url) {
 
-    onLoadStop: function(event) {
-        console.log('INAPP '+event.type + ' - ' + event.url);
-    },
+       try {
+         // target : _blank, _self, _system
+         var target = "_blank";
 
-    onExit: function(event) {
-    
-        console.log('INAPP '+event.type);
-        
-        if (iabRef) {
-            iabRef.removeEventListener('loadstart', onLoadStart);
-            iabRef.removeEventListener('loadstart', onLoadStop);
-            iabRef.removeEventListener('exit', onExit);
-            iabRef = null;
-        }
-        
-    } 
+         iabRef = window.open(url, target, "location=no");
 
-    
-  };
+         iabRef.addEventListener('loadstart', this.onLoadStart.bind(this));
+         iabRef.addEventListener('loadstop', this.onLoadStop.bind(this));
+         iabRef.addEventListener('exit', this.onExit.bind(this));
 
-  // registering geolocalisation only when cordova is ready.
-  Cordova.deviceready(function () {
-    Cordova.InApp = InApp;
-    
-        console.log("INAPP ready",Cordova.InApp);    
-    
-  });
-})(Cordova);
+       } catch (e) {
+         console.log('INAPP erreur ' + e + ' pour ' + url);
+       }
+     },
+
+     onLoadStart: function (event) {
+       console.log('INAPP ' + event.type + ' - ' + event.url);
+
+       //si url #success ou # error on kill
+       if (event.url.indexOf("success") != -1 || event.url.indexOf("error") != -1) {
+         console.log("INAPP SPECIAL DETECTE");
+
+         iabRef.close();
+       }
+     },
+
+     onLoadStop: function (event) {
+       console.log('INAPP ' + event.type + ' - ' + event.url);
+     },
+
+     onExit: function (event) {
+       console.log('INAPP ' + event.type);
+     }
+   };
+
+   // registering geolocalisation only when cordova is ready.
+   Cordova.deviceready(function () {
+     Cordova.InApp = InApp;
+
+     console.log("INAPP ready", Cordova.InApp);
+
+   });
+ })(Cordova);

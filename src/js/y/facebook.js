669caed578813d560ce01c558a499e05c9b1d14b
Marc Dassonneville <marc.dassonneville@gmail.com>
2013-03-15 20:42:34 +0100

    using new conf keys

--- src/js/y/facebook.js
+++ src/js/y/facebook.js
@@ -11,13 +11,21 @@
     connect: function () {
       // FIXME: dependance avec le player.
       var player = JSON.tryParse(window.localStorage.getItem("Y.Cache.Player"));
-      // building facebook url.
-      var facebookurl = Y.Conf.get("fb.url.login");
-      facebookurl = facebookurl.replace("[token]", player.token);
-      facebookurl = facebookurl.replace("[playerid]", player.id);
+      // build redirect url
+      var redirectUri = Y.Conf.get("fb.url.inappbrowser.redirect")
+                         .replace("[token]", player.token)
+                         .replace("[playerid]", player.id);
+      // build facebook oauth url
+      var facebookOauthUrl = Y.Conf.get("facebook.url.oauth")
+                              .replace("[fb_app_id]", Y.Conf.get("facebook.app.id"))
+                              .replace("[redirect_uri]", encodeURIComponent(redirectUri));
       // create a new browser
       var browser = new Cordova.InAppBrowser();
-      browser.open(facebookurl);
+      browser.open(facebookOauthUrl);
+
+      // https://www.facebook.com/dialog/oauth?%20client_id=408897482525651&scope=email
+      //  &redirect_uri=http%3A%2F%2Fplic.no-ip.org%3A9091%2Fv1%2Finappbrowser%2Fredirect.html%3Fplayerid%3D512e0e348e2859aa5100004e%26token%3D9801580
+      //  &response_type=token
     }
   };
 

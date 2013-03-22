17aecba646144056cc0f8bf137d14e17ca14546f
Marc Dassonneville <marcd@zescore.com>
2013-03-22 18:42:48 +0100

    UT: as a platform.

---
+++ platforms/ut/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export NODE_ENV="DEV"
+if [ -f ".port" ]
+then
+  utport=`cat .port | head -1`
+  echo ""
+  echo "using port number $utport from file .port for static client"
+  export YESWESCORE_UT_PORT=$utport
+  node static.js
+else
+  echo "  Please create .port file containing port number "
+  echo "  Exemple:  echo \"4000\" > .port "
+  exit;
+fi
+
+# launch test
+

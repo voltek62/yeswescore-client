9f2a242a40e48ba843be786534e4e938a431821d
Marc Dassonneville <marcd@zescore.com>
2013-03-23 13:18:59 +0100

    platforms: ut: tests launched with PORT* env variables

--- platforms/ut/test.sh
+++ platforms/ut/test.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
+BASEDIR=$(dirname $0)
+cd $BASEDIR;
+
+# are we in dev or in prod.
+if [ "$NODE_ENV" = "PROD" ]
+then
+  echo "you are in prod environment, you cannot launch tests"
+  exit 1
+fi
+
 export NODE_ENV="DEV"
 if [ -f ".port" ]
 then
   utport=`cat .port | head -1`
-  echo ""
+  fbport=`cat ../../../yeswescore-facebook/server/.port`
+  port=`cat ../../../yeswescore-server/server/.port`
   echo "using port number $utport from file .port for static client"
+  echo "using port number $fbport from file .port for yeswescore-facebook"
+  echo "using port number $port from file .port for yeswescore-server"
   export YESWESCORE_UT_PORT=$utport
-  node static.js
+  export YESWESCORE_FACEBOOK_PORT=$fbport
+  export YESWESCORE_PORT=$port
+  casperjs --disable-we-security $1
+  if [ $? -ne 0 ]
+  then
+    echo ""
+    echo "( usage: ./test.sh test/dev/launch.js )"
+  fi
 else
   echo "  Please create .port file containing port number "
   echo "  Exemple:  echo \"4000\" > .port "
   exit;
 fi
-
-# launch test
-

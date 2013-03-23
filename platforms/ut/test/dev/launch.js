887d9d71f6d8335f9c754328751b3747722830e9
Marc Dassonneville <marcd@zescore.com>
2013-03-23 13:18:54 +0100

    platforms: ut: tests launched with PORT* env variables

--- platforms/ut/test/dev/launch.js
+++ platforms/ut/test/dev/launch.js
@@ -1,4 +1,23 @@
-var casper = require('casper').create();
+// we are in casperjs (phantomjs environment)
+var casper = require('casper').create()
+  , system = require('system');
 
-var port = parseInt(process.env.YESWESCORE_UT_PORT, 10) || "4000";
-casper.start('http://localhost:'
+var utPort = parseInt(system.env.YESWESCORE_UT_PORT, 10);
+var fbPort = parseInt(system.env.YESWESCORE_FACEBOOK_PORT, 10);
+var port = parseInt(system.env.YESWESCORE_PORT, 10);
+
+var baseUrl = "http://localhost:"+utPort+"/";
+
+casper.start(baseUrl, function() {
+  console.log('started');
+  var UT = this.evaluate(function () { return window.UT; });
+  console.log("UT = " + UT);
+});
+
+casper.then(function() {
+  console.log('then');
+});
+
+casper.run(function() {
+  this.echo('test');
+});

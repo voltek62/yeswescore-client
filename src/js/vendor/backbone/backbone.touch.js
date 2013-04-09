0a4c71a546b8870afff1f1c3959e61e47ea56b9a
vincent <vincent@zescore.com>
2013-04-09 16:35:26 +0200

    add message for loading player

--- src/js/vendor/backbone/backbone.touch.js
+++ src/js/vendor/backbone/backbone.touch.js
@@ -17,6 +17,13 @@
 
     "use strict";
 
+    // The `getValue` and `delegateEventSplitter` is copied from 
+    // Backbones source, unfortunately these are not available
+    // in any form from Backbone itself
+    var getValue = function(object, prop) {
+        if (!(object && object[prop])) return null;
+        return _.isFunction(object[prop]) ? object[prop]() : object[prop];
+    };
     var delegateEventSplitter = /^(\S+)\s*(.*)$/;
 
     _.extend(Backbone.View.prototype, {
@@ -24,8 +31,7 @@
 
         touchPrevents : true,
 
-        isTouch : ('ontouchstart' in document && !('callPhantom' in window)) ||
-                  window.navigator.msPointerEnabled,
+        isTouch : 'ontouchstart' in document && !('callPhantom' in window),
 
         // Drop in replacement for Backbone.View#delegateEvent
         // Enables better touch support
@@ -34,38 +40,32 @@
         // event with listening for touch(start|move|end) in order to
         // quickly trigger touch taps
         delegateEvents: function(events) {
-            if (!(events || (events = _.result(this, 'events')))) return;
+            if (!(events || (events = getValue(this, 'events')))) return;
             this.undelegateEvents();
-
-            for (var key in events) {
-              var method = events[key];
-              if (!_.isFunction(method)) method = this[events[key]];
-              if (!method) throw new Error('Method "' + events[key] + '" does not exist');
-              var match = key.match(delegateEventSplitter);
-              var eventName = match[1], selector = match[2];
-              method = _.bind(method, this);
-
-              var that = this;
-              var boundHandler = function (e) {
-                  that._touchHandler(e, {method:method});
-              };
-              if (this.isTouch && eventName === 'click') {
-                if (window.navigator.msPointerEnabled) {
-                    this.$el.on('MSPointerDown' + suffix, selector, boundHandler);
-                    this.$el.on('MSPointerUp' + suffix, selector, boundHandler);
+            var suffix = '.delegateEvents' + this.cid;
+            _(events).each(function(method, key) {
+                if (!_.isFunction(method)) method = this[events[key]];
+                if (!method) throw new Error('Method "' + events[key] + '" does not exist');
+                var match = key.match(delegateEventSplitter);
+                var eventName = match[1], selector = match[2];
+                var boundHandler = _.bind(this._touchHandler,this);
+                method = _.bind(method, this);
+                if (this.isTouch && eventName === 'click') {
+                    this.$el.on('touchstart' + suffix, selector, boundHandler);
+                    this.$el.on('touchend' + suffix, selector,
+                        {method:method},
+                        boundHandler
+                    );
                 }
-                this.$el.on('touchstart' + suffix, selector, boundHandler);
-                this.$el.on('touchend' + suffix, selector, boundHandler);
-              } else {
-                eventName += '.delegateEvents' + this.cid;
-                // backbone code.
-                if (selector === '') {
-                  this.$el.on(eventName, method);
-                } else {
-                  this.$el.on(eventName, selector, method);
+                else {
+                    eventName += suffix;
+                    if (selector === '') {
+                        this.$el.bind(eventName, method);
+                    } else {
+                        this.$el.on(eventName, selector, method);
+                    }
                 }
-              }
-            }
+            }, this);
         },
 
         // At the first touchstart we register touchevents as ongoing
@@ -77,23 +77,7 @@
         // The `touchPrevents` toggle decides if Backbone.touch
         // will stop propagation and prevent default
         // for *button* and *a* elements
-        _touchHandler : function(e, data) {
-            if (window.navigator.msPointerEnabled) { // ie10.
-                switch (e.type) {
-                    case 'MSPointerUp':
-                        if (this.touchPrevents) {
-                            var tagName = e.currentTarget.tagName;
-                            if (tagName === 'BUTTON' ||
-                                tagName === 'A') {
-                                e.preventDefault();
-                                e.stopPropagation();
-                            }
-                        }
-                        data.method(e);
-                        break;
-                }
-                return;
-            }
+        _touchHandler : function(e) {
             if (!('changedTouches' in e.originalEvent)) return;
             var touch = e.originalEvent.changedTouches[0];
             var x = touch.clientX;
@@ -117,7 +101,7 @@
                                 e.stopPropagation();
                             }
                         }
-                        data.method(e);
+                        e.data.method(e);
                     }
                     break;
             }

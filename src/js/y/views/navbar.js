b2d599eab2b70898f34b2010f6a55d6d2b66963a
voltek62 <vincent@zescore.com>
2013-04-11 11:42:56 +0200

    hide/show navbar

--- src/js/y/views/navbar.js
+++ src/js/y/views/navbar.js
@@ -11,6 +11,18 @@ Y.Views.Navbar = Backbone.View.extend({
     // nothing yet
   },
   render: function () { },
+  
+  show: function () { 
+	this.el.show();
+  },
+
+  hide: function () { 
+    /*
+    this.$el.css("opacity", 0)
+	#navbar { transition: opacity ease 1s; }
+    */
+	this.el.hide();
+  },
 
   goToGames: function () { console.log('goToGames'); Y.Router.navigate("", {trigger: true}); },
   goToGamesAdd: function () { console.log('goToGamesAdd'); Y.Router.navigate("games/add", {trigger: true}); },

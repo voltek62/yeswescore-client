a4c4c3c7773dd78224643d23a33fd05e07e447fe
Marc Dassonneville <marc.dassonneville@gmail.com>
2013-04-11 02:47:55 +0200

    no cache (jquery)

--- src/js/y/y.js
+++ src/js/y/y.js
@@ -38,6 +38,7 @@
           // adding cors
           options.crossDomain = true;
           /*#endif*/
+          options.cache = false; // forcing jquery to add a random parameter.
           // calling jquery
           console.log('Backbone.ajax: '+url+' '+JSON.stringify(options));
           return $.ajax(url, options);

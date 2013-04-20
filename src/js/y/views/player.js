2111ef33a6b7397c8c5ee75f54163db135b0340c
vincent <vincent@zescore.com>
2013-04-20 18:46:31 +0200

    player view without pictos

--- src/js/y/views/player.js
+++ src/js/y/views/player.js
@@ -2,7 +2,8 @@ Y.Views.Player = Y.View.extend({
   el:"#content",
 
   events: {
-    'click #followButton': 'followPlayer'
+    'click #followButton': 'followPlayer',
+    'click #lastResult': 'lastResult'
   },
 
   pageName: "player",
@@ -36,6 +37,15 @@ Y.Views.Player = Y.View.extend({
     //change
     this.player.on( 'sync', this.render, this );
   },
+  
+  lastResult : function(elmt) {
+ 
+    console.log('lastResult ',elmt.currentTarget.href); 
+    var ref = elmt.currentTarget.href;
+    //console.log('listPlayer '+ref);
+	Y.Router.navigate(ref, {trigger: true}); 
+	   
+  },  
 
   followPlayer: function() {
   
@@ -53,6 +63,8 @@ Y.Views.Player = Y.View.extend({
           
           $('span.success').html('Vous ne suivez plus ce joueur').show();
           $("#followButton").text("Suivre");
+          $('#followButton').removeClass('button-selected');
+          $('#followButton').addClass('button'); 
 
           this.follow = 'false';
 
@@ -72,6 +84,9 @@ Y.Views.Player = Y.View.extend({
 
           $('span.success').html('Vous suivez ce joueur').show();
           $("#followButton").text("Ne plus suivre");
+          $('#followButton').removeClass('button');
+          $('#followButton').addClass('button-selected');          
+          
 
           this.follow = 'true';
 

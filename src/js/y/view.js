b51e3ec27506626e076a067d0b5d726c0f330dbe
Marc Dassonneville <marc.dassonneville@gmail.com>
2013-04-20 22:39:08 +0200

    inputMode & mycall handled in view abstract

--- src/js/y/view.js
+++ src/js/y/view.js
@@ -3,7 +3,26 @@
   "use strict";
   /*#endif*/
 
+  var events = {
+    'focus input': 'inputModeOn',
+    'blur input': 'inputModeOff',
+    'focus textarea': 'inputModeOn',
+    'blur textarea': 'inputModeOff',
+    'click *[data-js-call]': 'mycall'
+  };
+
   var View = Backbone.View.extend({
+    initialize: function () {
+      // merging this.events with events.
+      this.events = _.assign(events, this.events || {});
+      // proxy func call.
+      return this.myinitialize.apply(this, arguments);
+    },
+
+    mycall: function (e) {
+      this[$(e.currentTarget).attr("data-js-call")](e);
+    },
+
     inputModeOn: function () {
       Y.GUI.inputMode(true);
       return true;

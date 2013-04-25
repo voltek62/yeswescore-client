215d7849f02bb48ae015d2007613664d4d6cdd61
Marc Dassonneville <marc.dassonneville@gmail.com>
2013-04-25 16:47:23 +0200

    facebook share, fewer permissions

--- src/js/y/conf.js
+++ src/js/y/conf.js
@@ -64,7 +64,7 @@
           this.set("api.url.reports.clubs", apiBaseUrl + "/v1/report/clubs/");
           this.set("fb.url.inappbrowser.redirect", fbBaseUrl + "/v1/inappbrowser/redirect.html");
           this.set("facebook.app.id", fbAppId);
-          this.set("facebook.url.oauth", "https://www.facebook.com/dialog/oauth?%20client_id=[fb_app_id]&scope=email,read_stream,publish_stream,offline_access&redirect_uri=[redirect_uri]&response_type=token");
+          this.set("facebook.url.oauth", "https://www.facebook.com/dialog/oauth?%20client_id=[fb_app_id]&scope=email,publish_stream,offline_access&redirect_uri=[redirect_uri]&response_type=token");
           /*#endif*/
           break;
         case Y.Env.PROD:
@@ -81,7 +81,7 @@
           this.set("api.url.reports.clubs", "http://api.yeswescore.com/v1/report/clubs/");
           this.set("fb.url.inappbrowser.redirect", "https://fb.yeswescore.com/v1/inappbrowser/redirect.html");
           this.set("facebook.app.id", "447718828610668");
-          this.set("facebook.url.oauth", "https://www.facebook.com/dialog/oauth?%20client_id=[fb_app_id]&scope=email,read_stream,publish_stream,offline_access&redirect_uri=[redirect_uri]&response_type=token");
+          this.set("facebook.url.oauth", "https://www.facebook.com/dialog/oauth?%20client_id=[fb_app_id]&scope=email,publish_stream,offline_access&redirect_uri=[redirect_uri]&response_type=token");
           break;
         default:
           break;

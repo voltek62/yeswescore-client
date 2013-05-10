342bb277f2bc12f932d85eacd73b6320b971a2fc
Marc Dassonneville <marc.dassonneville@gmail.com>
2013-05-10 23:00:34 +0200

    #48868365 - when server is down (no network while loading bootstrap), asking the user to try later / reload

--- src/js/y/y.js
+++ src/js/y/y.js
@@ -75,7 +75,7 @@
                  // error handling.
                  //  if err is "deprecated" => we stop loading.
                  //  if err is other (ex: "network connection"), we continue to load.
-                 if (err && err == "deprecated")
+                 if (err == "deprecated" || err == "network error")
                    return callback(err);
                  // internationalization.
                  var i18nOptions = { lng: "fr-FR" };
@@ -139,7 +139,8 @@
                   return; // we do not want to continue loading.
                 }
                 if (err === "network error")
-                  42; // FIXME: we should listen to Y.Connection.
+                  Y.GUI.diplayErrorBootstrap();
+                  return;
               }
               // We are now ready.
               this.status = "ready";

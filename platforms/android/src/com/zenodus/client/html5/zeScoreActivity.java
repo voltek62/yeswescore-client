7746099e5503a5b4e48330aee2cb94caebcab772
vincent <vincent@zescore.com>
2013-05-13 00:08:05 +0200

    add splash android

--- platforms/android/src/com/zenodus/client/html5/zeScoreActivity.java
+++ platforms/android/src/com/zenodus/client/html5/zeScoreActivity.java
@@ -4,20 +4,9 @@ import android.os.Bundle;
 import org.apache.cordova.*;
 import com.zenodus.client.html5.R;
 
-import org.apache.cordova.api.CordovaInterface;
-import android.content.Intent;
-import android.net.Uri;
-
-import android.webkit.ValueCallback;
-
 
 public class zeScoreActivity extends DroidGap
 {
-	
-    private ValueCallback<Uri> mUploadMessage;
-    private final static int FILECHOOSER_RESULTCODE = 1;
-
-
 	
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -25,42 +14,14 @@ public class zeScoreActivity extends DroidGap
         super.onCreate(savedInstanceState);
 
         //super.setStringProperty("loadingDialog", "Starting your app...");
+        System.out.println("java forever");
+        
         super.setIntegerProperty("splashscreen", R.drawable.splash);
         
-        super.loadUrl(Config.getStartUrl(), 10000);
-
+        super.loadUrl(Config.getStartUrl(), 4000);
+        
     }
     
     
- // openFileChooser is an overridable method in WebChromeClient which isn't
-    // included in the SDK's Android stub code
-    public class FileAttachmentChromeClient extends CordovaChromeClient {
-
-        public FileAttachmentChromeClient(CordovaInterface ctx, CordovaWebView app) {
-            super(ctx, app);
-
-        }
-
-        // For Android > 3.x
-        public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType) {
-            mUploadMessage = uploadMsg;
-            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-            i.addCategory(Intent.CATEGORY_OPENABLE);
-            i.setType("*/*");
-
-            zeScoreActivity.this.startActivityForResult(Intent.createChooser(i, "Choose type of attachment"), FILECHOOSER_RESULTCODE);
-
-        }
-
-        // For Android < 3.x
-        public void openFileChooser(ValueCallback<Uri> uploadMsg) {
-            mUploadMessage = uploadMsg;
-            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-            i.addCategory(Intent.CATEGORY_OPENABLE);
-            i.setType("*/*");
-            zeScoreActivity.this.startActivityForResult(Intent.createChooser(i, "Choose type of attachment"), FILECHOOSER_RESULTCODE);
-        }
-
-    }
-    
+   
 }

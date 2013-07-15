d4bc0490be01c6fd8af8c05c7f1acf61ece94af6
vincent <vincent@zescore.com>
2013-07-15 20:01:54 +0200

    client add false notification for chrome

--- src/js/y/conf.js
+++ src/js/y/conf.js
@@ -122,14 +122,7 @@
         // loading permanent keys
         //  stored inside yws.json using format [{key:...,value:...,metadata:...},...]
         Cordova.ready(function () {
-          //On active les pushs
-          Cordova.Push.registerEvent(function (data) {
-          	console.log("push init "+data);
-          });
-          Cordova.Push.pushEvent(function (data) {
-          	console.log("push on reçoit "+data);
-          });
-        
+       
           Cordova.File.read(filename, function (err, data) {
             if (err)
               return onKeysLoaded(err); // FIXME
@@ -150,6 +143,17 @@
           onKeysLoaded();
         });
       }
+      
+      Cordova.ready(function () {
+      //On active les pushs
+        Cordova.Push.registerEvent(function (data) {
+          console.log("push init "+data);
+        });
+        Cordova.Push.pushEvent(function (data) {
+          console.log("push on reçoit "+data);
+        });
+      });
+      
     },
 
 

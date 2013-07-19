633626acb7c80a44f916b30c6d9bd2e8855d7a1f
vincent <vincent@zescore.com>
2013-07-19 17:32:12 +0200

    client : add urbanairship plugins

--- platforms/android/src/com/zenodus/client/html5/zeScoreActivity.java
+++ platforms/android/src/com/zenodus/client/html5/zeScoreActivity.java
@@ -5,6 +5,7 @@ import org.apache.cordova.*;
 import com.zenodus.client.html5.R;
 import com.urbanairship.AirshipConfigOptions;
 import com.urbanairship.UAirship;
+import com.urbanairship.phonegap.plugins.PushNotificationPluginIntentReceiver;
 import com.urbanairship.push.PushManager;
 
 public class zeScoreActivity extends DroidGap
@@ -18,30 +19,28 @@ public class zeScoreActivity extends DroidGap
         super.loadUrl(Config.getStartUrl(), 2000);
 
         //Notification
+        /*
         AirshipConfigOptions options = AirshipConfigOptions.loadDefaultOptions(this);
         options.developmentAppKey = "Ua5gAbxKRxqh2YYJufhA3A";
         options.productionAppKey = "Ua5gAbxKRxqh2YYJufhA3A";
-        options.inProduction = false; //determines which app key to use
-        
+        options.inProduction = false; //determines which app key to use       
         UAirship.takeOff(this.getApplication(), options);
-        PushManager.enablePush();   
+        PushManager.enablePush();  
+        */
+        
+        UAirship.takeOff(this.getApplication());
+        if (UAirship.shared().getAirshipConfigOptions().pushServiceEnabled) {
+            PushManager.enablePush();
+            PushManager.shared().setIntentReceiver(PushNotificationPluginIntentReceiver.class);
+        }        
         
         //String apid = PushManager.shared().getAPID();
         //System.out.println("My Application onCreate - App APID: " + apid);
     }
     
     
-    @Override
-    public void onStart() {
-        super.onStart();
-        UAirship.shared().getAnalytics().activityStarted(this);
-    }
-    
-    @Override
     public void onStop() {
-        super.onStop();
-        UAirship.shared().getAnalytics().activityStopped(this);
-    }    
-    
+      UAirship.land();
+    }
       
 }

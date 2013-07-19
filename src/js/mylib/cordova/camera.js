43eee11a80d68eb8fb08fa422339c6bd9ca38c77
Marc Dassonneville <marc.dassonneville@gmail.com>
2013-07-19 23:37:48 +0200

    cordova: camera: refactoring to return object instead of string to avoid memory duplication

--- src/js/mylib/cordova/camera.js
+++ src/js/mylib/cordova/camera.js
@@ -5,43 +5,32 @@
 
   // wrapper around cordova geolocation
   var Camera = {
-    
-    capturePhoto: function (callback) {   
-
+    capturePhoto: function (callback) {
       navigator.camera.getPicture(
         function Cordova_Camera_Success(imageData) {
-          if (imageData)
-            callback(imageData)
-          else
-            callback(null);
+          callback(null, {dataUri: imageData});
         },
         function Cordova_Camera_Error() {
-          callback(null);
+          callback("unknown camera error");
         },
         { quality: 50, destinationType: navigator.camera.DestinationType.DATA_URL }
-       );     
-      },
-      
+      );     
+    },
+    
+    // unused.
     getPhoto: function (callback) {  
-       
       navigator.camera.getPicture(
         function Cordova_Camera_Success(imageData) {
-          if (imageData)
-            callback(imageData)
-          else
-            callback(null);
+          callback(null, {dataUri: imageData});
         },
         function Cordova_Camera_Error() {
-          callback(null);
+          callback("unknown camera error");
         },
         //pictureSource.PHOTOLIBRARY
         //pictureSource.SAVEDPHOTOALBUM
         { quality: 50, destinationType: navigator.camera.DestinationType.FILE_URI, sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY }
        );     
-      }
-      
-      
-      
+     }
   };
 
   // registering geolocalisation only when cordova is ready.

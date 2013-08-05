6d951314e83d5241461cd23fed2dedadba4097df
vincent <vincent@zescore.com>
2013-08-05 16:28:37 +0200

    client refacto Cordova.device

--- src/js/mylib/cordova/device.js
+++ src/js/mylib/cordova/device.js
@@ -10,20 +10,31 @@
   // wrapper around cordova device 
   //  will be overrided by fake cordova in dev.
   var Device = {
-    name: window.device.name,
-    cordova: window.device.cordova,
-    platform: window.device.platform,
-    uuid: window.device.uuid,
-    version: window.device.version,
-    model: window.device.model,
+    name: '',
+    cordova: '',
+    platform: '',
+    uuid: '',
+    version: '',
+    model: '',
     // custom properties
     isGingerbread: isGingerbread,
     isIOS: isIOS,
     isAndroid : isAndroid    
   };
-
+  
   // registering geolocalisation only when cordova is ready.
   Cordova.deviceready(function () {
+  
+    if (typeof window.device !== "undefined") {
+      Device.name     = window.device.name;
+      Device.cordova  = window.device.cordova;
+      Device.platform = window.device.platform;
+      Device.uuid     = window.device.uuid;
+      Device.version  = window.device.version;
+      Device.model    = window.device.model;
+    }  
+  
     Cordova.Device = Device;
+  
   });
 })(Cordova);

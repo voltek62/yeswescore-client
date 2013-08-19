7272cb08012b6a5451f30cf24f7421fc143771d9
Marc Dassonneville <marc.dassonneville@gmail.com>
2013-08-19 12:52:04 +0200

    clean code => tabs => spaces

--- src/js/y/image.js
+++ src/js/y/image.js
@@ -16,36 +16,35 @@
     },
 
 
-	/**
-	 * Detecting vertical squash in loaded image.
-	 * Fixes a bug which squash image vertically while drawing into canvas for some images.
-	 * This is a bug in iOS6 devices. This function from https://github.com/stomita/ios-imagefile-megapixel
-	 * 
-	 */
-	detectVerticalSquash: function(img) {
-	    var iw = img.naturalWidth, ih = img.naturalHeight;
-	    var canvas = document.createElement('canvas');
-	    canvas.width = 1;
-	    canvas.height = ih;
-	    var ctx = canvas.getContext('2d');
-	    ctx.drawImage(img, 0, 0);
-	    var data = ctx.getImageData(0, 0, 1, ih).data;
-	    // search image edge pixel position in case it is squashed vertically.
-	    var sy = 0;
-	    var ey = ih;
-	    var py = ih;
-	    while (py > sy) {
-	        var alpha = data[(py - 1) * 4 + 3];
-	        if (alpha === 0) {
-	            ey = py;
-	        } else {
-	            sy = py;
-	        }
-	        py = (ey + sy) >> 1;
-	    }
-	    var ratio = (py / ih);
-	    return (ratio===0)?1:ratio;
-	},
+    /**
+    * Detecting vertical squash in loaded image.
+    * Fixes a bug which squash image vertically while drawing into canvas for some images.
+    * This is a bug in iOS6 devices. This function from https://github.com/stomita/ios-imagefile-megapixel
+    */
+    detectVerticalSquash: function(img) {
+      var iw = img.naturalWidth, ih = img.naturalHeight;
+      var canvas = document.createElement('canvas');
+      canvas.width = 1;
+      canvas.height = ih;
+      var ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0);
+      var data = ctx.getImageData(0, 0, 1, ih).data;
+      // search image edge pixel position in case it is squashed vertically.
+      var sy = 0;
+      var ey = ih;
+      var py = ih;
+      while (py > sy) {
+        var alpha = data[(py - 1) * 4 + 3];
+        if (alpha === 0) {
+          ey = py;
+        } else {
+          sy = py;
+        }
+        py = (ey + sy) >> 1;
+      }
+      var ratio = (py / ih);
+      return (ratio===0)?1:ratio;
+    },
 
     // this function resize a dataUrl image
     //

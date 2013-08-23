2b9e6423b027b661ee4a25d5f1017bef41ce4f0e
vincent <vincent@zescore.com>
2013-08-24 01:05:03 +0200

    client new subbar

--- src/js/y/views/navbar.js
+++ src/js/y/views/navbar.js
@@ -1,10 +1,14 @@
 Y.Views.Navbar = Y.View.extend({
   el: "#navbar",
-
+  
   events: {
-    'vclick a[data-fragment="games/list"]': "goToGames",
-    'vclick a[data-fragment="games/add"]' : "goToGamesAdd",
-    'vclick a[data-fragment="add"]'       : "goToAdd",    
+    'vclick a[data-fragment="follow"]': "displaySubBarFollow",    
+    'vclick a[data-fragment="add"]'  : "displaySubBarAdd",
+    
+    'vclick #subnavbar div[data-fragment="player"]': "goToPlayer",
+    'vclick #subnavbar div[data-fragment="team"]': "goToTeam",
+    'vclick #subnavbar div[data-fragment="club"]': "goToClub",         
+      
     'vclick a[data-fragment="account"]'   : "goToAccount"
   },
 
@@ -13,15 +17,16 @@ Y.Views.Navbar = Y.View.extend({
     Y.Router.on("pageChanged", function (page, fragment) {
       this.highlight(fragment);
     }, this);
+    
   },
   render: function () { },
   
   highlight: function (fragment) {
     // factorizing fragment.
-    if ( fragment == "games/list" || fragment == "games" ) {
-      fragment = "games/list";
-    } else if (fragment == "games/add") {
-      fragment = "games/add";
+    if ( fragment == "follow" || fragment == "games/list" || fragment == "teams/list" || fragment == "clubs/list" ) {
+      fragment = "follow";
+    } else if (fragment == "add" || fragment == "games/add" || fragment == "teams/add" || fragment == "clubs/add" ) {
+      fragment = "add";
     } else if (fragment == "account") {
       fragment = "account"
     } else {
@@ -36,19 +41,34 @@ Y.Views.Navbar = Y.View.extend({
   },
 
   show: function () { 
-	  this.$el.show();
+    this.$el.show();
   },
 
   hide: function () { 
-      /*
-      this.$el.css("opacity", 0)
-	  #navbar { transition: opacity ease 1s; }
-      */
-	  this.$el.hide();
+    /*
+    this.$el.css("opacity", 0)
+	#navbar { transition: opacity ease 1s; }
+    */
+    this.$el.hide();
   },
 
-  goToAdd: function () { Y.Router.navigate("add", {trigger: true}); },  
-  goToGames: function () { Y.Router.navigate("games/list", {trigger: true}); },
-  goToGamesAdd: function () { Y.Router.navigate("games/add", {trigger: true}); },
-  goToAccount: function () { Y.Router.navigate("account", {trigger: true}); }
+
+  displaySubBarFollow: function () { this.highlight("follow");$('#subnavbar').show(); },   
+  displaySubBarAdd: function () { this.highlight("add");$('#subnavbar').show(); },  
+  
+  goToPlayer: function () { 
+    console.log('goToPlayer');
+    this.$('div[data-fragment="player"]').addClass("highlighted");
+    Y.Router.navigate("games/list", {trigger: true}); 
+  },
+  goToTeam: function () { 
+    console.log('goToTeam');  
+    Y.Router.navigate("teams/list", {trigger: true}); 
+  },
+  goToClub: function () { 
+    console.log('goToClub');  
+    Y.Router.navigate("clubs/list", {trigger: true}); 
+  },  
+  
+  goToAccount: function () { $('#subnavbar').hide();Y.Router.navigate("account", {trigger: true}); }
 });
